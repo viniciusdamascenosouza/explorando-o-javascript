e36e15a14d8935e72c436b819cbc1f22d7545ae6
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-11 12:13:23 -0300

    introdução a linguagem

---
+++ 1-meu-primeiro-programa.js
@@ -0,0 +1,18 @@
+console.log("Meu primeiro programa: Trabalhando com variáveis");
+
+const idade = 20;
+
+console.log(idade);
+
+// somando e transformando em uma string
+const idadeSomada = String(idade + 2)
+console.log(idadeSomada);
+
+// ALGUMAS OPERAÇÕES:
+const idadeDividida = idade / 2;
+const idadeMultiplicada = idade * 2;
+const idadeSubtraida = idade - 2;
+
+console.log(idadeDividida);
+console.log(idadeMultiplicada);
+console.log(idadeSubtraida);
