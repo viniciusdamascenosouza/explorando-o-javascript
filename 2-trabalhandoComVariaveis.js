83833812724638ed981595ce8b8ee515ffe3f514
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-23 07:57:13 -0300

    trabalhando com variaveis

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,10 @@
+console.log("Trabalhando com variáveis");
+// JavaScript é uma linguagem CASE SENSITIVE
+
+idade = 20;
+Idade = 30;
+console.log(idade);
+console.log(Idade);
+
+const nome = "Ricardo";
+let ano = 2023;
