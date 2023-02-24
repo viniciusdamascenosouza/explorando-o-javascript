2d5f309f63d0e37290b2cf8c687ea0c63936dba2
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-24 07:15:41 -0300

    conversoes de tipos

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,13 @@
+console.log("conversão de tipos");
+
+console.log("ano" + 2020);
+console.log("2" + "2");
+console.log(parseInt("2") + parseInt("2"));
+
+
+console.log("10" / "2");
+console.log("Ricardo" / "2"); //Not a number = NaN
+
+console.log(20 / 3); 
+// números decimais são chamados de pontos flutuantes.
+// números decimais são escritos com ponto(.) ao invés de vírgulas(,)
