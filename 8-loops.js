37c2a2719fecf618dbe00b651788eff834153d10
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-27 17:56:59 -0300

    Loops

---
+++ 8-loops.js
@@ -0,0 +1,22 @@
+console.log(`Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "Rio de Janeiro";
+
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
+
+const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
+
+let contador = 0;
+
+while (condition) {
+    
+}
