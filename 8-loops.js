d94a8c3c365afa9cc2714842b53017232c5cff74
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2023-02-27 18:03:19 -0300

    While

--- 8-loops.js
+++ 8-loops.js
@@ -17,6 +17,11 @@ const podeComprar = idadeComprador >= 18 || estaAcompanhada == true;
 
 let contador = 0;
 
-while (condition) {
-    
+while (contador < 3) {
+    if(listaDeDestinos[contador] == destino) {
+        console.log("Destino existe.")
+    } else{
+        console.log("Destino não existe!")
+    }
+    contador ++;
 }
